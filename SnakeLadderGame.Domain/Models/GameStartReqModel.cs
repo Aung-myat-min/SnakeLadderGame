95040b1a426d73d2582bec0fee4e4696ce23c85d
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 21:01:20 +0630

    feat: game start req model

---
+++ SnakeLadderGame.Domain/Models/GameStartReqModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame.Domain.Models
+{
+    public class GameStartReqModel
+    {
+        public int NumberOfPlayers { get; set; }
+        public int BoardId { get; set; }
+    }
+}
