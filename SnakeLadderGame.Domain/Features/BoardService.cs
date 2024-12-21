6028dc7ea37711968574654c67376570e4e95dcc
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 21:03:35 +0630

    feat: room serive interface

--- SnakeLadderGame.Domain/Features/BoardService.cs
+++ SnakeLadderGame.Domain/Features/BoardService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SnakeLadderGame.Database.Models;
+using SnakeLadderGame.Domain.Interfaces;
 using SnakeLadderGame.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -7,86 +8,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SnakeLadderGame.Domain.Features
+namespace SnakeLadderGame.Domain.Features;
+
+public class BoardService : IBoardService
 {
-    public class BoardService
+    private readonly AppDBContext _db;
+
+    public BoardService(AppDBContext db)
     {
-        private readonly AppDBContext _db;
+        _db = db;
+    }
 
-        public BoardService(AppDBContext db)
+    public async Task<Result<BoardResponseModel>> CreateBoard(TblBoard newBoard)
+    {
+        var response = new Result<BoardResponseModel>();
+
+        await _db.TblBoards.AddAsync(newBoard);
+        int result = await _db.SaveChangesAsync();
+
+        if (result == 0)
         {
-            _db = db;
+            response = Result<BoardResponseModel>.Error("Failed to create board");
+            goto Result;
         }
 
-        public async Task<Result<BoardResponseModel>> CreateBoard(TblBoard newBoard)
+        var b = new BoardRouteResponseModel
         {
-            var response = new Result<BoardResponseModel>();
+            board = newBoard
+        };
+        response = Result<BoardResponseModel>.Success("Board created successfully", new BoardResponseModel { Board = b });
 
-            await _db.TblBoards.AddAsync(newBoard);
-            int result = await _db.SaveChangesAsync();
+    Result:
+        return response;
+    }
 
-            if (result == 0)
-            {
-                response = Result<BoardResponseModel>.Error("Failed to create board");
-                goto Result;
-            }
+    public async Task<Result<BoardResponseModel>> GetBoards()
+    {
+        var response = new Result<BoardResponseModel>();
 
-            var b = new BoardRouteResponseModel
-            {
-                board = newBoard
-            };
-            response = Result<BoardResponseModel>.Success("Board created successfully", new BoardResponseModel { Board = b });
+        var list = await _db.TblBoards.AsNoTracking().ToListAsync();
 
-        Result:
-            return response;
+        if (list.Count == 0)
+        {
+            response = Result<BoardResponseModel>.NotFound("No boards found");
+            goto Result;
         }
 
-        public async Task<Result<BoardResponseModel>> GetBoards()
+        var boards = list.Select(l => new BoardRouteResponseModel
         {
-            var response = new Result<BoardResponseModel>();
+            board = l,
+            routes = _db.TblBoardRoutes.Where(r => r.BoardId == l.BoardId).ToList()
+        }).ToList();
 
-            var list = await _db.TblBoards.AsNoTracking().ToListAsync();
+        response = Result<BoardResponseModel>.Success("Here are the avaliable boards!", new BoardResponseModel { LBoards = boards });
 
-            if(list.Count == 0)
-            {
-                response = Result<BoardResponseModel>.NotFound("No boards found");
-                goto Result;
-            }
+    Result:
+        return response;
+    }
 
-            var boards = list.Select(l => new BoardRouteResponseModel
-            {
-                board = l,
-                routes = _db.TblBoardRoutes.Where(r => r.BoardId == l.BoardId).ToList()
-            }).ToList();
+    public async Task<Result<BoardResponseModel>> GetBoard(int boardId)
+    {
+        var response = new Result<BoardResponseModel>();
 
-            response = Result<BoardResponseModel>.Success("Here are the avaliable boards!", new BoardResponseModel { LBoards = boards });
+        var item = await _db.TblBoards.AsNoTracking().Where(b => b.BoardId == boardId).FirstOrDefaultAsync();
 
-        Result:
-            return response;
+        if (item is null)
+        {
+            response = Result<BoardResponseModel>.NotFound("No board found");
+            goto Result;
         }
 
-        public async Task<Result<BoardResponseModel>> GetBoard(int boardId)
+        var board = new BoardRouteResponseModel
         {
-            var response = new Result<BoardResponseModel>();
+            board = item,
+            routes = _db.TblBoardRoutes.Where(r => r.BoardId == item.BoardId).ToList()
+        };
 
-            var item = await _db.TblBoards.AsNoTracking().Where(b => b.BoardId == boardId).FirstOrDefaultAsync();
+        response = Result<BoardResponseModel>.Success("Here are the avaliable boards!", new BoardResponseModel { Board = board });
 
-            if (item is null)
-            {
-                response = Result<BoardResponseModel>.NotFound("No board found");
-                goto Result;
-            }
-
-            var board =  new BoardRouteResponseModel
-            {
-                board = item,
-                routes = _db.TblBoardRoutes.Where(r => r.BoardId == item.BoardId).ToList()
-            };
-
-            response = Result<BoardResponseModel>.Success("Here are the avaliable boards!", new BoardResponseModel { Board = board });
-
-        Result:
-            return response;
-        }
+    Result:
+        return response;
     }
 }
---
+++ SnakeLadderGame.Domain/Interfaces/IBoardService.cs
@@ -0,0 +1,12 @@
+﻿using SnakeLadderGame.Database.Models;
+using SnakeLadderGame.Domain.Models;
+
+namespace SnakeLadderGame.Domain.Interfaces
+{
+    public interface IBoardService
+    {
+        Task<Result<BoardResponseModel>> CreateBoard(TblBoard newBoard);
+        Task<Result<BoardResponseModel>> GetBoard(int boardId);
+        Task<Result<BoardResponseModel>> GetBoards();
+    }
+}
