6ca7a4025ef6a1ecad637bce93e08b9dcc73ac3b
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 16:44:59 +0630

    feat: game information show service

--- SnakeLadderGame.Domain/Features/GameRoomService.cs
+++ SnakeLadderGame.Domain/Features/GameRoomService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Azure;
+using Microsoft.EntityFrameworkCore;
 using SnakeLadderGame.Database.Models;
 using SnakeLadderGame.Domain.Models;
 using System;
@@ -24,7 +25,7 @@ namespace SnakeLadderGame.Domain.Features
         {
             var response = new Result<GameRoomResponseModel>();
 
-            if(numberOfPlayer < 2 || numberOfPlayer > 5)
+            if (numberOfPlayer < 2 || numberOfPlayer > 5)
             {
                 response = Result<GameRoomResponseModel>.ValidationErr("Number of player must be between 2 and 5!");
                 goto Result;
@@ -55,11 +56,11 @@ namespace SnakeLadderGame.Domain.Features
             }
 
             var players = new List<TblPlayer>();
-            for(int i = 1; i <= numberOfPlayer; i++)
+            for (int i = 1; i <= numberOfPlayer; i++)
             {
                 var player = new TblPlayer
                 {
-                    Color = GlobalData.playerColors[i -1],
+                    Color = GlobalData.playerColors[i - 1],
                     Position = 1,
                     Turn = i,
                     RoomCode = roomCode
@@ -80,9 +81,114 @@ namespace SnakeLadderGame.Domain.Features
                 gameRoom = newRoom,
                 players = players
             };
-            response = Result<GameRoomResponseModel>.Success( "Game Room Created!\nHere is the Game Room Code and Player Informations!", new GameRoomResponseModel { gameRoom = model });
+            response = Result<GameRoomResponseModel>.Success("Game Room Created!\nHere is the Game Room Code and Player Informations!", new GameRoomResponseModel { gameRoom = model });
 
         Result:
+            return response;
+        }
+
+        public async Task<Result<PlayResponseModel>> Play(string roomCode)
+        {
+            var respsonse = new Result<PlayResponseModel>();
+            var actions = new List<string>();
+
+            #region Check Game Room Avaliable or Ended
+            var gameRoom = await _db.TblGameRooms.Where(r => r.RoomCode == roomCode).FirstOrDefaultAsync();
+            if (gameRoom is null)
+            {
+                respsonse = Result<PlayResponseModel>.NotFound("Game Room not found!");
+                goto Result;
+            }
+            if (gameRoom.Winner != null)
+            {
+                respsonse = Result<PlayResponseModel>.NotFound("Game Room is ended!");
+                goto Result;
+            }
+            #endregion
+
+            #region Roll Dice
+            var dice = new Random();
+            var diceNumber = dice.Next(1, 7);
+            #endregion
+
+            #region Get Last Turn Player
+            var lastTurn = gameRoom.LastTurn;
+            var player = await _db.TblPlayers.Where(p => p.RoomCode == roomCode && p.Turn == lastTurn).FirstOrDefaultAsync();
+            if (player is null)
+            {
+                respsonse = Result<PlayResponseModel>.NotFound("Player not found!");
+                goto Result;
+            }
+            #endregion
+
+            #region Calcualte New Position
+            var board = _db.TblBoards.AsNoTracking().Where(b => b.BoardId == gameRoom.BoardId).FirstOrDefault();
+            var winningPosition = board!.BoardDimension * board.BoardDimension;
+
+            int? newPosition = player.Position + diceNumber;
+            actions.Add($"{player.Color} + has reached to {newPosition}");
+
+            if(newPosition > winningPosition)
+            {
+                newPosition = newPosition - (newPosition - winningPosition);
+                actions.Add($"Since Player have reached beyond final point, Player has to move back to {newPosition}");
+            }
+
+            if (newPosition < winningPosition)
+            {
+                var route = await _db.TblBoardRoutes.Where(r => r.BoardId == gameRoom.BoardId && r.Place == player.Position).FirstOrDefaultAsync();
+                if (route is null)
+                {
+                    respsonse = Result<PlayResponseModel>.NotFound("Route not found!");
+                    goto Result;
+                }
+
+                switch (route.ActionType)
+                {
+                    case "Ladder":
+                        newPosition = route.Destination;
+                        actions.Add($"Lucky! Player has found a ladder and moved to {newPosition}");
+                        break;
+                    case "Snake":
+                        newPosition = route.Destination;
+                        actions.Add($"Oops! Player has found a snake and moved to {newPosition}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            if (newPosition == winningPosition)
+            {
+                gameRoom.Winner = player.PlayerId;
+            }
+        #endregion
+
+
+        Result:
+            return respsonse;
+        }
+
+        public async Task<Result<string>> GetGameInformation(string roomCode)
+        {
+            var response = new Result<string>();
+
+            var game = await _db.TblGameRooms.AsNoTracking().Where(g => g.RoomCode == roomCode).FirstOrDefaultAsync();
+            if(game is null)
+            {
+                response = Result<string>.NotFound("Game Room not found!");
+                goto Result;
+            }
+
+            var players = await _db.TblPlayers.AsNoTracking().Where(p => p.RoomCode == roomCode).OrderByDescending(p => p.Position).ToListAsync();
+
+            string gameInformation = $"In Room: {roomCode}\n";
+            foreach (var player in players)
+            {
+                gameInformation += $"Player {player.Color} is at {player.Position}\n";
+            }
+
+            Result:
             return response;
         }
     }
---
+++ SnakeLadderGame.Domain/Models/PlayResponseModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderGame.Domain.Models
+{
+    public class PlayResponseModel
+    {
+        public List<string> ActionsMade { get; set; } = null!;
+        public string? Positions { get; set; }
+        public string? NextTurn { get; set; }
+    }
+}
