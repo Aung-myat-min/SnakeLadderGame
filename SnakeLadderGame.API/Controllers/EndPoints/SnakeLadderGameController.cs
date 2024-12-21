17245fc32c0da87209e8c92332ef0b30d41edd82
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 21:02:04 +0630

    feat: game room api controller

---
+++ SnakeLadderGame.API/Controllers/EndPoints/SnakeLadderGameController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SnakeLadderGame.Domain.Interfaces;
+using SnakeLadderGame.Domain.Models;
+
+namespace SnakeLadderGame.API.Controllers.EndPoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SnakeLadderGameController : BaseController
+    {
+        private readonly IGameRoomService _roomService;
+
+        public SnakeLadderGameController(IGameRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpPost("{players}")]
+        public async Task<IActionResult> StartANewGame(GameStartReqModel players)
+        {
+            try
+            {
+                var response = await _roomService.CreateGR(players);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("{roomCode}")]
+        public async Task<IActionResult> PlayGame(string roomCode)
+        {
+            try
+            {
+                var response = await _roomService.Play(roomCode);
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
