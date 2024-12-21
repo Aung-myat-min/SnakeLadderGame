2d39596bc36c624cc515ba8de74eb6edef64a93a
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 21:11:30 +0630

    feat: applied dependency injection in API

---
+++ SnakeLadderGame.API/Controllers/EndPoints/BoardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SnakeLadderGame.Domain.Interfaces;
+
+namespace SnakeLadderGame.API.Controllers.EndPoints
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BoardController : BaseController
+    {
+        private readonly IBoardService _boardService;
+
+        public BoardController(IBoardService boardService)
+        {
+            _boardService = boardService;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetBoards()
+        {
+            try
+            {
+                var response = await _boardService.GetBoards();
+                return Execute(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- SnakeLadderGame.API/Program.cs
+++ SnakeLadderGame.API/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using SnakeLadderGame.Database.Models;
+using SnakeLadderGame.Domain.Features;
+using SnakeLadderGame.Domain.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDBContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+}, ServiceLifetime.Transient, ServiceLifetime.Transient);
+
+builder.Services.AddScoped<IGameRoomService, GameRoomService>();
+builder.Services.AddScoped<IBoardService, BoardService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
