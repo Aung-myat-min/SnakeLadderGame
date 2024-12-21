892705eb35175db594aad9491768eecfcd3a6134
Aung-Myat-Min <koaungmyatmin0@gmail.com>
2024-12-21 20:45:41 +0630

    feat: api base controller

---
+++ SnakeLadderGame.API/Controllers/BaseController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SnakeLadderGame.Domain.Models;
+
+namespace SnakeLadderGame.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        [NonAction]
+        public IActionResult Execute<T>(Result<T> result)
+        {
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                if (result.IsValidationError || result.IsNormalError)
+                {
+                    return BadRequest(result);
+                }
+                else if (result.IsNotFoundError)
+                {
+                    return NotFound(result);
+                }
+                else if (result.IsServerError)
+                {
+                    return StatusCode(500, result);
+                }
+
+                return StatusCode(500, "Result Pattern Not Applied!");
+            }
+        }
+    }
+}
